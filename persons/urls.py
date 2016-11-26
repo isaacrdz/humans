b7ee45cc38bbd0ec36029fd41a8e1c5c0f2de42c
isaacrdz <zack_cpp@hotmail.com>
2016-11-26 00:48:18 -0600

    test

--- persons/urls.py
+++ persons/urls.py
@@ -2,5 +2,5 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^persons/$', views.person_list, name= 'person_list' ),
+    url(r'^$', views.person_list, name= 'person_list' ),
 ]
